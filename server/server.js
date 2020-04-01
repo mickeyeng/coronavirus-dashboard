6f8bb3a5084eb9ad7435ab0b8ac7f2e433b1bde6
mickey english <mickeyeng92@gmail.com>
2020-04-01 12:54:24 +0100

    nodemon working

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 3000;
 const path = require('path');
 
 // Set static folder
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.render('hello from server'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
