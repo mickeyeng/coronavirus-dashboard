ea0abd294be0e23f6afcb164a5ab7d8fae8d6f63
mickey english <mickeyeng92@gmail.com>
2020-04-02 13:18:14 +0100

    map showing with mapbox api key

--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,15 @@
+const map = document.querySelector('#mapid');
+
+const KEY =
+  'pk.eyJ1IjoibWlja2V5ZW5nIiwiYSI6ImNqdHQ5dHlqMjB3dm80ZGw4MW93bXJ3bDEifQ.XygC53CVuF2M4onZPwvwmg';
+
 export function displayMap({ lat, long }) {
   console.log('display-map working');
   console.log(lat, long);
-  const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  const mymap = L.map('mapid').setView([lat, long], 13);
+
   L.tileLayer(
-    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+    `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${KEY}`,
     {
       attribution:
         'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -11,10 +17,9 @@ export function displayMap({ lat, long }) {
       id: 'mapbox/streets-v11',
       tileSize: 512,
       zoomOffset: -1,
-      accessToken:
-        'pk.eyJ1IjoibWlja2V5ZW5nIiwiYSI6ImNrOGhsZDBucTAxbmQzbW1nM25pd3R1Ym8ifQ.gLQNi75K0Od_Li8bkJso0w'
+      accessToken: KEY
     }
   ).addTo(mymap);
 
-  var marker = L.marker([51.5, -0.09]).addTo(mymap);
+  var marker = L.marker([lat, long]).addTo(mymap);
 }
